4981f9aac23fcb791df31c59acbf43c47af6935e
onikun94 <onikun94@gmail.com>
2021-05-12 17:03:15 +0900

    abc199

--- abc199/b/main.py
+++ abc199/b/main.py
@@ -1,3 +1,14 @@
 N = int(input())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
+
+maxA = max(A)
+minB = min(B)
+
+if maxA > minB:
+    print(0)
+else:
+    count = 0
+    for i in range(maxA, minB + 1):
+        count += 1
+    print(count)
