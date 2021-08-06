437eeece4f8760809de5ad5bffdc4b623c9b7570
onikun94 <onikun94@gmail.com>
2021-08-06 21:37:32 +0900

    abc212

---
+++ abc212/a/main.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    a, b = map(int, input().split())
+    if 0 < a and b == 0:
+        print("Gold")
+    elif a == 0 and 0 < b:
+        print("Silver")
+    else:
+        print("Alloy")
